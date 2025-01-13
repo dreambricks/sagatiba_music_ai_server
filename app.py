9fb345de3bd4a2a423d9fb8d7114b14e00bcb6f0
Julio Cesar Balbino de Morais <julio4110@hotmail.com>
2025-01-13 16:17:26 -0300

    feat: generate test page to generate lyrics

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import logging
-from flask import Flask
+from flask import Flask, request, render_template
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -10,6 +10,28 @@ app = Flask(__name__)
 def alive():
     return "alive"
 
+@app.route("/generate-lyrics", methods=["GET", "POST"])
+def generate_lyrics():
+    if request.method == "POST":
+
+        phone = request.form.get("phone")
+        destination = request.form.get("destination")
+        invite_options = request.form.getlist("invite")
+        weekdays = request.form.getlist("weekday")
+        message = request.form.get("message")
+
+        print(f"""
+                Dados recebidos:
+                
+                Phone: {phone}
+                Destination: {destination}
+                Invite Options: {invite_options}
+                Weekdays: {weekdays}
+                Message: {message}
+                """)
+
+    return render_template("form.html")
+
 if __name__ == "__main__":
     logger.info("Starting Flask application...")
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=True)
